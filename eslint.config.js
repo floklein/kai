a4ca22b6354ff7338c920e1e0ea695c831649947
floklein <4128942+floklein@users.noreply.github.com>
2025-05-26 00:06:15 +0200

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,37 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import reactCompiler from "eslint-plugin-react-compiler";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config({ ignores: ["dist"] }, eslintConfigPrettier, {
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
-  files: ["**/*.{ts,tsx}"],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
+export default tseslint.config(
+  eslintConfigPrettier,
+  reactHooks.configs["recommended-latest"],
+  reactCompiler.configs.recommended,
+  js.configs.recommended,
+  reactRefresh.configs.vite,
+  tseslint.configs.recommended,
+  {
+    ignores: ["dist"],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      "react-compiler/react-compiler": "warn",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
   },
-  plugins: {
-    "react-hooks": reactHooks,
-    "react-refresh": reactRefresh,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        ignoreRestSiblings: true,
-      },
-    ],
-  },
-});
+);
