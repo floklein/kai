58722e56e812b1cab62b1498c054f08577bbfbd2
floklein <4128942+floklein@users.noreply.github.com>
2025-07-17 00:10:03 +0200

    updates

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -8,127 +8,88 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-// Import Routes
+import { Route as rootRouteImport } from './routes/__root'
+import { Route as NewRouteImport } from './routes/new'
+import { Route as ChatIdRouteImport } from './routes/$chatId'
+import { Route as IndexRouteImport } from './routes/index'
 
-import { Route as rootRoute } from "./routes/__root";
-import { Route as NewImport } from "./routes/new";
-import { Route as ChatIdImport } from "./routes/$chatId";
-import { Route as IndexImport } from "./routes/index";
-
-// Create/Update Routes
-
-const NewRoute = NewImport.update({
-  id: "/new",
-  path: "/new",
-  getParentRoute: () => rootRoute,
-} as any);
-
-const ChatIdRoute = ChatIdImport.update({
-  id: "/$chatId",
-  path: "/$chatId",
-  getParentRoute: () => rootRoute,
-} as any);
-
-const IndexRoute = IndexImport.update({
-  id: "/",
-  path: "/",
-  getParentRoute: () => rootRoute,
-} as any);
-
-// Populate the FileRoutesByPath interface
-
-declare module "@tanstack/react-router" {
-  interface FileRoutesByPath {
-    "/": {
-      id: "/";
-      path: "/";
-      fullPath: "/";
-      preLoaderRoute: typeof IndexImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/$chatId": {
-      id: "/$chatId";
-      path: "/$chatId";
-      fullPath: "/$chatId";
-      preLoaderRoute: typeof ChatIdImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/new": {
-      id: "/new";
-      path: "/new";
-      fullPath: "/new";
-      preLoaderRoute: typeof NewImport;
-      parentRoute: typeof rootRoute;
-    };
-  }
-}
-
-// Create and export the route tree
+const NewRoute = NewRouteImport.update({
+  id: '/new',
+  path: '/new',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const ChatIdRoute = ChatIdRouteImport.update({
+  id: '/$chatId',
+  path: '/$chatId',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const IndexRoute = IndexRouteImport.update({
+  id: '/',
+  path: '/',
+  getParentRoute: () => rootRouteImport,
+} as any)
 
 export interface FileRoutesByFullPath {
-  "/": typeof IndexRoute;
-  "/$chatId": typeof ChatIdRoute;
-  "/new": typeof NewRoute;
+  '/': typeof IndexRoute
+  '/$chatId': typeof ChatIdRoute
+  '/new': typeof NewRoute
 }
-
 export interface FileRoutesByTo {
-  "/": typeof IndexRoute;
-  "/$chatId": typeof ChatIdRoute;
-  "/new": typeof NewRoute;
+  '/': typeof IndexRoute
+  '/$chatId': typeof ChatIdRoute
+  '/new': typeof NewRoute
 }
-
 export interface FileRoutesById {
-  __root__: typeof rootRoute;
-  "/": typeof IndexRoute;
-  "/$chatId": typeof ChatIdRoute;
-  "/new": typeof NewRoute;
+  __root__: typeof rootRouteImport
+  '/': typeof IndexRoute
+  '/$chatId': typeof ChatIdRoute
+  '/new': typeof NewRoute
 }
-
 export interface FileRouteTypes {
-  fileRoutesByFullPath: FileRoutesByFullPath;
-  fullPaths: "/" | "/$chatId" | "/new";
-  fileRoutesByTo: FileRoutesByTo;
-  to: "/" | "/$chatId" | "/new";
-  id: "__root__" | "/" | "/$chatId" | "/new";
-  fileRoutesById: FileRoutesById;
+  fileRoutesByFullPath: FileRoutesByFullPath
+  fullPaths: '/' | '/$chatId' | '/new'
+  fileRoutesByTo: FileRoutesByTo
+  to: '/' | '/$chatId' | '/new'
+  id: '__root__' | '/' | '/$chatId' | '/new'
+  fileRoutesById: FileRoutesById
+}
+export interface RootRouteChildren {
+  IndexRoute: typeof IndexRoute
+  ChatIdRoute: typeof ChatIdRoute
+  NewRoute: typeof NewRoute
 }
 
-export interface RootRouteChildren {
-  IndexRoute: typeof IndexRoute;
-  ChatIdRoute: typeof ChatIdRoute;
-  NewRoute: typeof NewRoute;
+declare module '@tanstack/react-router' {
+  interface FileRoutesByPath {
+    '/new': {
+      id: '/new'
+      path: '/new'
+      fullPath: '/new'
+      preLoaderRoute: typeof NewRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/$chatId': {
+      id: '/$chatId'
+      path: '/$chatId'
+      fullPath: '/$chatId'
+      preLoaderRoute: typeof ChatIdRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/': {
+      id: '/'
+      path: '/'
+      fullPath: '/'
+      preLoaderRoute: typeof IndexRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+  }
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
   ChatIdRoute: ChatIdRoute,
   NewRoute: NewRoute,
-};
-
-export const routeTree = rootRoute
-  ._addFileChildren(rootRouteChildren)
-  ._addFileTypes<FileRouteTypes>();
-
-/* ROUTE_MANIFEST_START
-{
-  "routes": {
-    "__root__": {
-      "filePath": "__root.tsx",
-      "children": [
-        "/",
-        "/$chatId",
-        "/new"
-      ]
-    },
-    "/": {
-      "filePath": "index.tsx"
-    },
-    "/$chatId": {
-      "filePath": "$chatId.tsx"
-    },
-    "/new": {
-      "filePath": "new.tsx"
-    }
-  }
 }
-ROUTE_MANIFEST_END */
+export const routeTree = rootRouteImport
+  ._addFileChildren(rootRouteChildren)
+  ._addFileTypes<FileRouteTypes>()
